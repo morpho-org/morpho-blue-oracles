502219f02ff2f113ab655b9c79d4b78baf2de9dd
Quentin Garchery <garchery.quentin@gmail.com>
2023-09-21 11:20:48 +0200

    docs: fix a few typos

--- src/chainlink/Oracle.sol
+++ src/chainlink/Oracle.sol
@@ -13,15 +13,15 @@ contract Oracle is IOracle {
     AggregatorV3Interface public immutable BASE_FEED;
     /// @notice Quote feed.
     AggregatorV3Interface public immutable QUOTE_FEED;
-    /// @notice Price scale factor. Automatically computer at contract creation.
+    /// @notice Price scale factor. Automatically computed at contract creation.
     uint256 public immutable SCALE_FACTOR;
 
     /* CONSTRUCTOR */
 
-    /// @param baseFeed Base feed. Pass address zero for price = 1.
-    /// @param baseTokenDecimals Base token decimals. Pass 0 if the price is one.
-    /// @param quoteFeed Quote feed. Pass address zero for price = 1.
-    /// @param quoteTokenDecimals Quote token decimals. Pass 0 if the price is one.
+    /// @param baseFeed Base feed. Pass address zero if the price = 1.
+    /// @param baseTokenDecimals Base token decimals. Pass 0 if the price = 1.
+    /// @param quoteFeed Quote feed. Pass address zero if the price = 1.
+    /// @param quoteTokenDecimals Quote token decimals. Pass 0 if the price = 1.
     constructor(
         AggregatorV3Interface baseFeed,
         uint256 baseTokenDecimals,
@@ -42,8 +42,8 @@ contract Oracle is IOracle {
         return _feedPrice(BASE_FEED) * SCALE_FACTOR / _feedPrice(QUOTE_FEED);
     }
 
-    /// @dev Performing some security checks and returns the lateste price of a feed.
-    /// @dev When feed = address(0), returns 1.
+    /// @dev Performing some security checks and returns the latest price of a feed.
+    /// @dev When feed is the address 0, returns 1.
     function _feedPrice(AggregatorV3Interface feed) private view returns (uint256) {
         if (address(feed) == address(0)) return 1;
         (, int256 answer,,,) = feed.latestRoundData();
@@ -52,6 +52,7 @@ contract Oracle is IOracle {
     }
 
     /// @dev Returns feed.decimals() if feed != address(0), else returns 0.
+
     function _feedDecimals(AggregatorV3Interface feed) private view returns (uint256) {
         if (address(feed) == address(0)) return 0;
         return feed.decimals();
