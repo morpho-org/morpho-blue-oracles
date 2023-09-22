6a7d3bb3d9fd4ac19f54f0ec30ad2f3e3652ca22
Quentin Garchery <garchery.quentin@gmail.com>
2023-09-22 17:40:44 +0200

    feat: add oracle for productive non rebasing tokens

---
+++ src/chainlink/OracleNonRebasing.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {IOracle} from "morpho-blue/interfaces/IOracle.sol";
+
+import {AggregatorV3Interface, DataFeedLib} from "./libraries/DataFeedLib.sol";
+
+interface ERC4626 {
+    function convertToAssets(uint256) external view returns (uint256);
+}
+
+contract OracleNonRebasing is IOracle {
+    using DataFeedLib for AggregatorV3Interface;
+
+    /* CONSTANT */
+
+    /// @notice Vault.
+    ERC4626 public immutable VAULT;
+    /// @notice Vault decimals.
+    uint256 public immutable VAULT_DECIMALS;
+    /// @notice Base feed.
+    AggregatorV3Interface public immutable BASE_FEED;
+    /// @notice Quote feed.
+    AggregatorV3Interface public immutable QUOTE_FEED;
+    /// @notice Price scale factor. Automatically computed at contract creation.
+    uint256 public immutable SCALE_FACTOR;
+
+    /* CONSTRUCTOR */
+
+    /// @param vault Vault.
+    /// @param baseFeed Base feed.
+    /// @param quoteFeed Quote feed. Pass address zero if the price = 1.
+    /// @param vaultDecimals Vault decimals.
+    /// @param quoteTokenDecimals Quote token decimals. Pass 0 if the price = 1.
+    constructor(
+        ERC4626 vault,
+        AggregatorV3Interface baseFeed,
+        AggregatorV3Interface quoteFeed,
+        uint256 vaultDecimals,
+        uint256 quoteTokenDecimals
+    ) {
+        VAULT = vault;
+        BASE_FEED = baseFeed;
+        QUOTE_FEED = quoteFeed;
+        VAULT_DECIMALS = vaultDecimals;
+        SCALE_FACTOR = 10 ** (36 + quoteFeed.wrapDecimals() - baseFeed.wrapDecimals() - quoteTokenDecimals);
+    }
+
+    /* PRICE */
+
+    /// @inheritdoc IOracle
+    function price() external view returns (uint256) {
+        return (VAULT.convertToAssets(10 ** VAULT_DECIMALS) * BASE_FEED.wrapPrice() * SCALE_FACTOR)
+            / QUOTE_FEED.wrapPrice();
+    }
+}
---
+++ test/chainlink/OracleNonRebasingTest.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "src/chainlink/OracleNonRebasing.sol";
+import "src/chainlink/libraries/ErrorsLib.sol";
+
+// 18 decimals of precision
+AggregatorV3Interface constant daiEthFeed = AggregatorV3Interface(0x773616E4d11A78F511299002da57A0a94577F1f4);
+// 18 decimals of precision
+AggregatorV3Interface constant usdcEthFeed = AggregatorV3Interface(0x986b5E1e1755e3C2440e960477f25201B0a8bbD4);
+
+ERC4626 constant sDaiVault = ERC4626(0x83F20F44975D03b1b09e64809B757c47f942BEeA);
+
+contract OracleTest is Test {
+    function setUp() public {
+        vm.selectFork(vm.createFork(vm.envString("ETH_RPC_URL")));
+    }
+
+    function testSdaiEthOracle() public {
+        OracleNonRebasing oracle =
+            new OracleNonRebasing(sDaiVault, daiEthFeed, AggregatorV3Interface(address(0)), 18, 0);
+        (, int256 expectedPrice,,,) = daiEthFeed.latestRoundData();
+        assertEq(oracle.price(), sDaiVault.convertToAssets(1e18) * uint256(expectedPrice) * 10 ** (36 + 0 - 18 - 0));
+    }
+}
