3d88fd5884fdb09af3ec1044192b023bbb8c2430
MathisGD <mathis@morpho.xyz>
2023-10-04 14:52:10 +0200

    docs: minor improvements

--- src/chainlink/OracleTwoFeeds.sol
+++ src/chainlink/OracleTwoFeeds.sol
@@ -31,13 +31,14 @@ contract OracleTwoFeeds is IOracle {
     ) {
         BASE_FEED = baseFeed;
         QUOTE_FEED = quoteFeed;
-        // We note pB the base price, and pQ the quote price (price of 1e(decimals) asset).
-        // We want to return 1e36 * (pB/1e(bDecimals) / (pQ/1e(qDecimals)).
-        // Chainlink returns pB * bFeedPrecision and pQ * qFeedPrecision.
-        // So we have 1e36 * (pB/1e(bDecimals) / (pQ/1e(qDecimals) = pB * 1e(bFeedPrecision) * SCALE_FACTOR / (pQ *
-        // 1e(qFeedPrecision))
+        // Let pB be the base price, and pQ the quote price (price of 1e(decimals) asset).
+        // Chainlink feeds return pB * bFeedPrecision and pQ * qFeedPrecision.
+        // `price()` should return 1e36 * (pB/1e(bDecimals) / (pQ/1e(qDecimals)).
+        // Yet `price()` returns pB * 1e(bFeedPrecision) * SCALE_FACTOR / (pQ * 1e(qFeedPrecision))
+        // So 1e36 * (pB/1e(bDecimals) / (pQ/1e(qDecimals) = pB*1e(bFeedPrecision) * SCALE_FACTOR /
+        // (pQ*1e(qFeedPrecision))
         // So SCALE_FACTOR = 1e36 / 1e(bDecimals) * 1e(qDecimals) * 1e(qFeedPrecision) / 1e(bFeedPrecision)
-        // So SCALE_FACTOR = 1e(36 + qDecimals + qFeedPrecision - bDecimals - bFeedPrecision)
+        //                 = 1e(36 + qDecimals + qFeedPrecision - bDecimals - bFeedPrecision)
         SCALE_FACTOR =
             10 ** (36 + quoteTokenDecimals + quoteFeed.getDecimals() - baseFeed.getDecimals() - baseTokenDecimals);
     }
