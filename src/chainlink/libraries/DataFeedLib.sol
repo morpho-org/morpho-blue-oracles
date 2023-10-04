a6668131f6a7c742fe36e6fd07ecb0b69fb68eae
MathisGD <74971347+MathisGD@users.noreply.github.com>
2023-10-04 14:55:05 +0200

    docs: minor improvements
    Co-authored-by: Romain Milon <rmilon@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/chainlink/libraries/DataFeedLib.sol
+++ src/chainlink/libraries/DataFeedLib.sol
@@ -5,7 +5,7 @@ import {ErrorsLib} from "./ErrorsLib.sol";
 import {AggregatorV3Interface} from "../interfaces/AggregatorV3Interface.sol";
 
 library DataFeedLib {
-    /// @dev Performing some security checks and returns the latest price of a feed.
+    /// @dev Performs some safety checks and returns the latest price of a feed.
     /// @dev When `feed` is the address zero, returns 1.
     function getPrice(AggregatorV3Interface feed) internal view returns (uint256) {
         if (address(feed) == address(0)) return 1;
@@ -14,7 +14,8 @@ library DataFeedLib {
         return uint256(answer);
     }
 
-    /// @dev Returns `feed.decimals()` when `feed` is not the address zero, else returns 0.
+    /// @dev Returns the number of decimals of a feed.
+    /// @dev When `feed` is the address zero, returns 0.
     function getDecimals(AggregatorV3Interface feed) internal view returns (uint256) {
         if (address(feed) == address(0)) return 0;
         return feed.decimals();
