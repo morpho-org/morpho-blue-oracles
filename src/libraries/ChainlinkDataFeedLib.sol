7b8d0c4214d961c859acacccc3a50e7b6615facc
Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
2023-10-13 14:34:26 +0200

    docs: apply suggestions
    Co-authored-by: Quentin Garchery <QGarchery@users.noreply.github.com>
    Signed-off-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>

--- src/libraries/ChainlinkDataFeedLib.sol
+++ src/libraries/ChainlinkDataFeedLib.sol
@@ -13,12 +13,11 @@ library ChainlinkDataFeedLib {
     /// @dev Performs safety checks and returns the latest price of a `feed`.
     /// @dev When `feed` is the address zero, returns 1.
     /// @dev Notes on safety checks:
-    /// - A stale period is not checked since the heartbeat of a Chainlink feed is an offchain parameter that can
-    /// change any time and checking the price deviation onchain is not possible. It was not recommended by the Chailink
+    /// - Staleness is not checked since the heartbeat of a Chainlink feed is an offchain parameter that can
+    /// change at any time and checking the price deviation onchain is not possible. It was not recommended by the Chailink
     /// team either.
     /// - The price is not checked to be in the min/max bounds since the check is already performed by Chainlink most of
-    /// the time. Adding
-    /// a safety margin would be arbitrary as well since the contract is immutable.
+    /// the time. Adding a safety margin would be arbitrary as well since the contract is immutable.
     /// - No fallback is used. In case the oracle reverts, Morpho Blue users can still exit their positions but
     /// can't be liquidated.
     function getPrice(AggregatorV3Interface feed) internal view returns (uint256) {
