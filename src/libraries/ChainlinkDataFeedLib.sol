d9a21c7ac0050c4ad47123c7a324f842f1ac9482
MerlinEgalite <egalite.merlin@gmail.com>
2023-10-13 14:35:23 +0200

    docs: format + remove CL mention

--- src/libraries/ChainlinkDataFeedLib.sol
+++ src/libraries/ChainlinkDataFeedLib.sol
@@ -14,8 +14,7 @@ library ChainlinkDataFeedLib {
     /// @dev When `feed` is the address zero, returns 1.
     /// @dev Notes on safety checks:
     /// - Staleness is not checked since the heartbeat of a Chainlink feed is an offchain parameter that can
-    /// change at any time and checking the price deviation onchain is not possible. It was not recommended by the Chailink
-    /// team either.
+    /// change at any time and checking the price deviation onchain is not possible.
     /// - The price is not checked to be in the min/max bounds since the check is already performed by Chainlink most of
     /// the time. Adding a safety margin would be arbitrary as well since the contract is immutable.
     /// - No fallback is used. In case the oracle reverts, Morpho Blue users can still exit their positions but
