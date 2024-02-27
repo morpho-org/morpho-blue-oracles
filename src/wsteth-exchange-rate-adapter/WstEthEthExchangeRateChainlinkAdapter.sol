3743f80d0ff104572d405e9e4d43d9b75e8c1e5e
MerlinEgalite <egalite.merlin@gmail.com>
2024-02-27 15:58:11 +0100

    refactor: use stETH instead of wstETH

--- src/wsteth-exchange-rate-adapter/WstEthEthExchangeRateChainlinkAdapter.sol
+++ src/wsteth-exchange-rate-adapter/WstEthEthExchangeRateChainlinkAdapter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.21;
 
-import {IWstEth} from "./interfaces/IWstEth.sol";
+import {IStEth} from "./interfaces/IStEth.sol";
 import {MinimalAggregatorV3Interface} from "./interfaces/MinimalAggregatorV3Interface.sol";
 
 import {ErrorsLib} from "./libraries/ErrorsLib.sol";
@@ -15,17 +15,17 @@ contract WstEthEthExchangeRateChainlinkAdapter is MinimalAggregatorV3Interface {
     uint8 public constant decimals = 18;
     string public constant description = "wstETH/ETH exchange rate";
 
-    IWstEth public immutable WST_ETH;
+    IStEth public immutable ST_ETH;
 
-    constructor(address wstEth) {
-        require(wstEth != address(0), ErrorsLib.ZERO_ADDRESS);
+    constructor(address stEth) {
+        require(stEth != address(0), ErrorsLib.ZERO_ADDRESS);
 
-        WST_ETH = IWstEth(wstEth);
+        ST_ETH = IStEth(stEth);
     }
 
     /// @dev Silently overflows if `stEthPerToken` is greater than `type(int256).max`.
     function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80) {
-        // It is assumed that `stEthPerToken` returns a price with 18 decimals precision.
-        return (0, int256(WST_ETH.stEthPerToken()), 0, 0, 0);
+        // It is assumed that `getPooledEthByShares` returns a price with 18 decimals precision.
+        return (0, int256(ST_ETH.getPooledEthByShares(1 ether)), 0, 0, 0);
     }
 }
